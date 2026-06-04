5b9fe9696ae918f2b4af0b6b057f8ef68e747979
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 12:18:33 +0530

    Update User_Login.py

--- Class_work/iteration_4thJune/User_Login.py
+++ Class_work/iteration_4thJune/User_Login.py
@@ -1,1 +1,9 @@
+#Program for user login
+while True:
+    password = str(input("Enter Password: "))
 
+    if password == "admin123":
+        print("Login Successful.")
+        break
+    else:
+        print("Invalid Password.")
