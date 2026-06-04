4c2a0852beb2043066e6f23fa3eab2a5ceaa0368
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 12:09:43 +0530

    Implement ATM PIN validation loop

--- Class_work/iteration_4thJune/atmPIN.py
+++ Class_work/iteration_4thJune/atmPIN.py
@@ -1,1 +1,11 @@
+#program for atm pin 
+while True:
+    pin = int(input("Enter PIN: "))
 
+    if pin == 1234:
+        print("Access Granted.")
+        break
+    else:
+        print("Incorrect PIN. Try Again.")
+        print()
+        
