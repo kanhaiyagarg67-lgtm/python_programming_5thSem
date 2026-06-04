c4b9fbdaa36c1c1299b7cc77df4a53b210e09281
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 14:51:33 +0530

    Update number_guessing.py

--- labwork/iteration_4th_june_lab/number_guessing.py
+++ labwork/iteration_4th_june_lab/number_guessing.py
@@ -1,1 +1,25 @@
+import random
 
+# Generate secret number between 1 and 50
+secret_number = random.randint(1, 50)
+
+# Variable to count attempts
+attempts = 0
+
+# Loop will run until correct guess
+while True:
+    guess = int(input("Enter your guess: "))
+
+    # Increase attempt count
+    attempts = attempts + 1
+
+    if guess > secret_number:
+        print("Too High")
+
+    elif guess < secret_number:
+        print("Too Low")
+
+    else:
+        print("Correct Guess")
+        print("Total Attempts:", attempts)
+        break
