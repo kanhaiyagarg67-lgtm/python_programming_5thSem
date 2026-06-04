dcae307e35a8e55dce8ed7af0810cd1ef1b1aa1c
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 14:38:58 +0530

    Add Armstrong number checker in armstrong.py
    Implement Armstrong number check based on user input.

--- labwork/iteration_4th_june_lab/armstrong.py
+++ labwork/iteration_4th_june_lab/armstrong.py
@@ -1,1 +1,28 @@
+# Take input from user
+num = int(input("Enter a number: "))
 
+# Store original number in temp variable
+temp = num
+
+# Count total digits in the number
+digits = len(str(num))
+
+# Variable to store sum of powers of digits
+sum = 0
+
+# Loop runs until all digits are separated
+while temp > 0:
+    # Get last digit of the number
+    digit = temp % 10
+
+    # Add digit raised to power of total digits
+    sum = sum + digit ** digits
+
+    # Remove last digit from number
+    temp = temp // 10
+
+# Check whether calculated sum is equal to original number
+if sum == num:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
