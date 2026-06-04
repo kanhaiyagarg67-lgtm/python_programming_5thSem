d2034cade438efeec7b48466e13adfb1255c14fb
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 23:56:44 +0530

    program of mini employee payeroll system
    Added functionality to calculate net salary and determine employee grade based on net salary.

--- labwork/iteration_4th_june_lab/Mini_employee_payroll_system.py
+++ labwork/iteration_4th_june_lab/Mini_employee_payroll_system.py
@@ -1,1 +1,25 @@
+#program for mini employee payeroll system
+Employee_name = str(input("Enter Employee Name: "))
+print("Employee Name:", Employee_name)    
+Basic_salary = int(input("Enter Basic Salary: "))
+print("Basic Salary: ₹", Basic_salary)
 
+HRA = 0.2 * Basic_salary
+DA = 0.1 * Basic_salary
+PF = 0.12 * Basic_salary
+
+Gross_salary = Basic_salary + HRA + DA 
+print("Gross Salary: ₹", Gross_salary)
+
+net_salary = Gross_salary - PF
+print("Net Salary: ₹", net_salary)
+
+if net_salary > 50000:
+    GRADE = "Senior grade"
+
+elif net_salary > 30000:
+    GRADE = "Mid grade"
+
+else:
+    GRADE = "Junior grade"
+  print("Grade:", GRADE)
