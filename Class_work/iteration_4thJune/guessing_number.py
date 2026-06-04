872f53c751d49e35d4ac186f7528cf27f4bcface
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 13:48:09 +0530

    Add guessing number game implementation

--- Class_work/iteration_4thJune/guessing_number.py
+++ Class_work/iteration_4thJune/guessing_number.py
@@ -1,1 +1,8 @@
-
+#program for guessing the number game
+while True:
+    secret_number=int(input("guess the number :"))
+    if secret_number==7:
+        print("Congratulations! You guessed the number.")
+        break
+    else:
+        print("Wrong guess. Try again.")
