29d71323fa4781a61f013dd98484ec98aa008041
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 11:55:27 +0530

    Add attendance counting program for students

--- Class_work/iteration_4thJune/attendenceCounter.py
+++ Class_work/iteration_4thJune/attendenceCounter.py
@@ -1,1 +1,7 @@
-
+#program for counting the students in class
+student = 1
+while student <= 30:
+    print("Entered Student")
+    print("Attendence count:", student)
+    student = student + 1
+print("ALL STUDENTS ARE PRESENT")
