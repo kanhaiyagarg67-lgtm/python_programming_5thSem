73d030aeeda2488a3c39ce26b9ade6dfe82bec4e
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 21:50:59 +0530

    water tank filling program

--- Class_work/iteration_4thJune/Water_tank_filling.py
+++ Class_work/iteration_4thJune/Water_tank_filling.py
@@ -1,1 +1,6 @@
-
+ #Program for water tank filling
+water_level = 0  # initialy water level is 0 
+while water_level <= 100: # run until 100 litres
+    water_level += 10 # increase level by 10% in each minute
+    print("Water level: ", water_level, "litres")
+print("Water tank is full.")
