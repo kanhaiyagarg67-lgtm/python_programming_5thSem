c5d9528c2c2adec9f44baca0065d3725eaa148b7
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-06 11:15:13 +0530

    program of Flight Booking analysis

--- Class_work/tuple_6th_june/Flight_Booking_analysis.py
+++ Class_work/tuple_6th_june/Flight_Booking_analysis.py
@@ -1,43 +1,43 @@
-# Program for the Flight Booking Analysis 
-# Problem Statement 
-# A flight reservation system stores passenger records as tuples: 
-# bookings = ( 
-#     ("P101", "Delhi", "Confirmed"), 
-#     ("P102", "Mumbai", "Waiting"), 
-#     ("P103", "Delhi", "Confirmed"), 
-#     ("P104", "Chennai", "Cancelled"), 
-#     ("P105", "Mumbai", "Confirmed"), 
-#     ("P106", "Delhi", "Waiting") 
-# ) 
-# Where: 
-# • Passenger ID  
-# • Destination  
-# • Booking Status  
-# Tasks 
-# Write a Python program to: 
-# 1. Display all passengers whose booking status is Confirmed.  
-# 2. Count the number of passengers travelling to Delhi.  
-# 3. Count Confirmed, Waiting, and Cancelled bookings separately.  
-# 4. Create a list containing passenger IDs with Waiting status.  
-# 5. Determine which destination has the highest number of bookings.  
-# Sample Output 
-# Confirmed Passengers: 
-# P101 Delhi 
-# P103 Delhi 
-# P105 Mumbai 
+'''Program for the Flight Booking Analysis 
+ Problem Statement 
+ A flight reservation system stores passenger records as tuples: 
+ bookings = ( 
+     ("P101", "Delhi", "Confirmed"), 
+     ("P102", "Mumbai", "Waiting"), 
+     ("P103", "Delhi", "Confirmed"), 
+     ("P104", "Chennai", "Cancelled"), 
+     ("P105", "Mumbai", "Confirmed"), 
+     ("P106", "Delhi", "Waiting") 
+ ) 
+Where: 
+ • Passenger ID  
+ • Destination  
+ • Booking Status  
+ Tasks 
+ Write a Python program to: 
+ 1. Display all passengers whose booking status is Confirmed.  
+ 2. Count the number of passengers travelling to Delhi.  
+ 3. Count Confirmed, Waiting, and Cancelled bookings separately.  
+ 4. Create a list containing passenger IDs with Waiting status.  
+ 5. Determine which destination has the highest number of bookings.  
+ Sample Output 
+ Confirmed Passengers: 
+ P101 Delhi 
+ P103 Delhi 
+ P105 Mumbai 
  
-# Passengers Travelling to Delhi: 3 
+ Passengers Travelling to Delhi: 3 
  
-# Confirmed: 3 
-# Waiting: 2 
-# Cancelled: 1 
+ Confirmed: 3 
+ Waiting: 2 
+ Cancelled: 1 
  
-# Waiting List: 
-# ['P102', 'P106'] 
+ Waiting List: 
+ ['P102', 'P106'] 
  
-# Most Booked Destination: 
-# Delhi
-
+ Most Booked Destination: 
+ Delhi
+'''
 
 #Creating booking data
 bookings = (
